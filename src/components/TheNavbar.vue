f5e9ec11642809bb124ebe8b074f3a36f77aa9c8
williepotgieter <willie.potgieter@kurtosys.com>
2021-06-05 16:11:42 +0200

    Created basic navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar app tile dark color="grey darken-1">
+      <v-app-bar-title>
+        <div>LOGO</div>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <div class="d-none d-sm-flex">
+        <v-btn text tile class="mr-3">about</v-btn>
+        <v-btn text tile class="mr-3">skills</v-btn>
+        <v-btn text tile class="mr-3">portfolio</v-btn>
+        <v-btn text tile class="mr-3">contact</v-btn>
+      </div>
+      <v-app-bar-nav-icon class="d-sm-none"></v-app-bar-nav-icon>
+    </v-app-bar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TheNavbar",
+};
+</script>
